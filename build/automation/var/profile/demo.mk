9ca248d39d20361f61b324468da23be9a8e7ad76
Daniel Osae <86474960+daonhs@users.noreply.github.com>
2021-11-03 09:58:39 +0000

    SFD-3751 Addition of demo and prod profiles (#17)

---
+++ build/automation/var/profile/demo.mk
@@ -0,0 +1,27 @@
+-include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
+
+# ==============================================================================
+# Service variables
+AWS_CERTIFICATE := arn:aws:acm:eu-west-2:$(AWS_ACCOUNT_ID):certificate/c0718115-4e22-4f48-a4aa-8c16ea86c5e6
+ECR_URL := $(AWS_ECR)/$(PROJECT_GROUP_SHORT)/$(PROJECT_NAME_SHORT)/$(PROJECT_NAME)
+
+PROFILE := demo
+ENVIRONMENT := $(PROFILE)
+SPRING_PROFILES_ACTIVE := $(PROFILE)
+
+DEPLOYMENT_STACKS := application
+INFRASTRUCTURE_STACKS := service,datastore,lambda
+
+SERVER_PORT := 443
+IMAGE_TAG := v0.0.1
+
+POSTCODE_LOCATION_DYNAMO_URL := https://dynamodb.$(AWS_REGION).amazonaws.com
+DYNAMODB_POSTCODE_LOC_MAP_TABLE := $(PROJECT_GROUP_NAME_SHORT)-$(PROFILE)-postcode-location-mapping
+
+REPLICAS := 1
+PROJECT_GROUP_NAME_SHORT := $(PROJECT_GROUP_SHORT)-$(PROJECT_NAME_SHORT)
+TTL := 2d
+
+TF_VAR_service_prefix := $(PROJECT_GROUP_NAME_SHORT)-$(PROFILE)
+TF_VAR_postcode_mapping_dynamo_name := $(TF_VAR_service_prefix)-postcode-location-mapping
+TF_VAR_sf_resources_bucket := $(TF_VAR_service_prefix)-application-resources
