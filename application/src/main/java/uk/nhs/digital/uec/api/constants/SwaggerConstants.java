9fa28b839cd70380defb8f7cdea9b564ea1d3f26
Arif <arif.mohammed2@nhs.net>
2022-01-07 11:30:19 +0000

    SFD-3977 Define request attribute description in swagger

---
+++ application/src/main/java/uk/nhs/digital/uec/api/constants/SwaggerConstants.java
@@ -0,0 +1,10 @@
+package uk.nhs.digital.uec.api.constants;
+
+public class SwaggerConstants {
+
+  private SwaggerConstants() {}
+
+  public static final String POSTCODES_DESC =
+      "List of postcode/s to provide northing and easting location coordinates";
+  public static final String NAME_DESC = "Organisation name";
+}
