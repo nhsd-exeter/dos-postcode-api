72b259d9be10fdfad0cc4d0299b87370e3e91040
daniel osae <daniel.osae@nhs.net>
2022-03-23 17:15:45 +0000

    SFD-4227 Add non prod vars to demo file

--- build/automation/var/profile/demo.mk
+++ build/automation/var/profile/demo.mk
@@ -1,4 +1,6 @@
--include $(VAR_DIR)/platform-texas/v1/account-live-k8s-prod.mk
+-include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
+# -include $(VAR_DIR)/platform-texas/v1/account-live-k8s-prod.mk
+
 
 # ==============================================================================
 # Service variables
