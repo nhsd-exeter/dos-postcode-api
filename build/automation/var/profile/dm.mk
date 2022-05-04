82c5ad8c4e831b1be28c1143d80cf0c0681ee40d
adeniyiabdul <99728546+adeniyiabdul@users.noreply.github.com>
2022-05-04 15:00:01 +0100

    SFD-XXXX Docker registry live fix (#78)
    * SFD-XXXX Docker register live fix
    
    * SFD-XXXX Docker register live fix

--- build/automation/var/profile/dm.mk
+++ build/automation/var/profile/dm.mk
@@ -1,15 +1,15 @@
--include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
+-include $(VAR_DIR)/platform-texas/v1/account-live-k8s-prod.mk
 
 # ==============================================================================
 # Service variables
-AWS_CERTIFICATE := arn:aws:acm:eu-west-2:$(AWS_ACCOUNT_ID):certificate/c0718115-4e22-4f48-a4aa-8c16ea86c5e6
+AWS_CERTIFICATE := arn:aws:acm:eu-west-2:$(AWS_ACCOUNT_ID):certificate/8d65eee4-cf92-4a00-84de-7a9f544ba724
 ECR_URL := $(AWS_ECR)/$(PROJECT_GROUP_SHORT)/$(PROJECT_NAME_SHORT)/$(PROJECT_NAME)
 
-PROFILE := prod
+PROFILE := dm
 ENVIRONMENT := $(PROFILE)
 SPRING_PROFILES_ACTIVE := $(PROFILE)
 
-SLEEP_AFTER_PLAN := 2m
+SLEEP_AFTER_PLAN := 120s
 
 DEPLOYMENT_STACKS := application
 INFRASTRUCTURE_STACKS := $(INFRASTRUCTURE_STACK_STORE),$(INFRASTRUCTURE_STACKS_ETL)
@@ -24,22 +24,24 @@ DYNAMODB_POSTCODE_LOC_MAP_TABLE := $(PROJECT_GROUP_NAME_SHORT)-$(PROFILE)-postco
 
 REPLICAS := 3
 PROJECT_GROUP_NAME_SHORT := $(PROJECT_GROUP_SHORT)-$(PROJECT_NAME_SHORT)
+TTL := 1d
 
 TF_VAR_service_prefix := $(PROJECT_GROUP_NAME_SHORT)-$(PROFILE)
 TF_VAR_postcode_mapping_dynamo_name := $(TF_VAR_service_prefix)-postcode-location-mapping
 TF_VAR_sf_resources_bucket := $(TF_VAR_service_prefix)-application-resources
 TF_VAR_postcode_etl_logging_level := INFO
 TF_VAR_postcode_etl_sns_logging_level := INFO
-TF_VAR_postcode_etl_sns_email := postcode-etl-alerts-p-aaaae5ldnncapl2nsysfdnhbii@a2si.slack.com
+TF_VAR_postcode_etl_sns_email := postcode-etl-alerts-d-aaaafmi4x7xvayo2zzqr5sojpa@a2si.slack.com
 TF_VAR_postcode_etl_extract_alarm_period := 86400
 TF_VAR_postcode_etl_insert_alarm_period := 86400
 
-TF_VAR_dos_replica_db := uec-core-dos-live-db-12-replica-sf.dos-db-rds
-TF_VAR_dos_security_group := uec-core-dos-live-db-12-replica-sf-sg
+TF_VAR_sf_read_replica_db := uec-core-dos-put-db-replica-sf.dos-db-put
+TF_VAR_sf_read_replica_db_sg := uec-core-dos-put-db-12-replica-sf-sg
+# TF_VAR_dos_read_replica_secret_name := core-dos-uet-database-upgrade/deployment
 TF_VAR_dos_read_replica_secret_name := core-dos/deployment
 TF_VAR_dos_read_replica_key := DB_SF_READONLY_PASSWORD
 TF_VAR_postcode_etl_db_user := dos_sf_readonly
-TF_VAR_postcode_etl_source_db := pathwaysdos
+TF_VAR_postcode_etl_source_db := pathwaysdos_ut
 TF_VAR_core_dos_python_libs := core-dos-python-libs
 
 CERTIFICATE_DOMAIN := certificate
