eb823050c7fc01df4743aeab650a88c35f03ba36
adeniyiabdul <99728546+adeniyiabdul@users.noreply.github.com>
2022-05-16 13:49:31 +0100

    SFD-XXXX Updated the dmo profile file (#105)
    * SFD-XXXX Updated the dmo profile file
    
    * SFD-XXXX Updated the dmo profile file

--- build/automation/var/profile/dmo.mk
+++ build/automation/var/profile/dmo.mk
@@ -48,15 +48,16 @@ TF_VAR_postcode_etl_insert_alarm_period := 86400
 #TF_VAR_postcode_etl_source_db := pathwaysdos
 #TF_VAR_core_dos_python_libs := core-dos-python-libs
 
+
 # Connection to DoS Read Replica for extraction Lambdas. For the Demo env we point to the live read replica
-TF_VAR_sf_read_replica_db  := uec-core-dos-put-db-12-replica-sf.core-dos-db-put
+TF_VAR_sf_read_replica_db  := uec-core-dos-put-db-12-replica-sf
 # TF_VAR_sf_read_replica_db  := uec-core-dos-put-db-12-replica-sf.dos-db-rds
 TF_VAR_sf_read_replica_db_sg := uec-core-dos-put-db-12-replica-sf-sg
 TF_VAR_dos_read_replica_secret_name := core-dos-uet-database-upgrade/deployment
 # TF_VAR_dos_read_replica_secret_name := core-dos/deployment
 TF_VAR_dos_read_replica_key := DB_SF_READONLY_PASSWORD
 TF_VAR_postcode_etl_db_user := dos_sf_readonly
-TF_VAR_postcode_etl_source_db := pathwaysdos
+TF_VAR_postcode_etl_source_db := pathwaysdos_ut
 
 CERTIFICATE_DOMAIN := certificate
 CHECK_DEPLOYMENT_TIME_LIMIT := 600
