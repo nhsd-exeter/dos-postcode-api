d44cd736f40926b8674468e600b688ca4962f5ca
daniel osae <daniel.osae@nhs.net>
2022-07-11 22:16:27 +0100

    SFD-4377 Add regions to dynamodb

--- application/src/test/java/uk/nhs/digital/uec/api/utils/RegionUtilTest.java
+++ application/src/test/java/uk/nhs/digital/uec/api/utils/RegionUtilTest.java
@@ -1,5 +1,16 @@
 package uk.nhs.digital.uec.api.utils;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,32 +23,18 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.nhs.digital.uec.api.domain.RegionRecord;
 import uk.nhs.digital.uec.api.util.RegionUtil;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(SpringExtension.class)
 public class RegionUtilTest {
 
-
-  @InjectMocks
-  RegionUtil classUnderTest;
-  @Mock
-  ResourceLoader resourceLoader;
+  @InjectMocks RegionUtil classUnderTest;
+  @Mock ResourceLoader resourceLoader;
   InputStream inputStream;
   Resource resource;
-  final static String path = "classpath:postcode_regions.csv";
-  final static String mockCSV = "Postcode,Region,Nearby districts,UK region\n" +
-    "EX8,Exeter,\"EX9, EX7, EX3, EX5, TQ14, EX2, EX1, EX10, EX4, EX11\",South West";
+  static final String path = "classpath:postcode_regions.csv";
+  static final String mockCSV =
+      "Postcode,Region,Nearby districts,UK region\n"
+          + "EX8,Exeter,\"EX9, EX7, EX3, EX5, TQ14, EX2, EX1, EX10, EX4, EX11\",South West";
 
   @BeforeEach
   public void setup() {
@@ -47,31 +44,29 @@ public class RegionUtilTest {
 
   @Test
   public void getCSVToModelListSuccess() throws IOException {
-    //given
+    // given
     when(resourceLoader.getResource(path)).thenReturn(resource);
     when(resource.getInputStream()).thenReturn(inputStream);
 
-    //when
+    // when
     List<RegionRecord> results = classUnderTest.call();
 
-    //Then
+    // Then
     assertFalse(results.isEmpty());
-
   }
 
   @Test
   public void throwIOExceptionDuringCSVRead() throws IOException {
-    //Given
+    // Given
     when(resourceLoader.getResource(path)).thenReturn(resource);
     when(resource.getInputStream()).thenThrow(IOException.class);
 
-    //when
+    // when
     List<RegionRecord> results = classUnderTest.call();
 
-    //Then
+    // Then
     verify(resourceLoader).getResource("classpath:postcode_regions.csv");
     verify(resource).getInputStream();
     assertTrue(results.isEmpty());
   }
 }
-
