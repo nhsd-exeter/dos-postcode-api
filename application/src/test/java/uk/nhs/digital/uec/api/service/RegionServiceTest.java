f9c58eee645575d70b0d5be6455f84b581266e31
Rohit Kondapalli <rohit.kondapalli1@nhs.net>
2022-07-25 12:05:07 +0100

    Refactor the code and add new endpoint

--- application/src/test/java/uk/nhs/digital/uec/api/service/RegionServiceTest.java
+++ application/src/test/java/uk/nhs/digital/uec/api/service/RegionServiceTest.java
@@ -1,5 +1,6 @@
 package uk.nhs.digital.uec.api.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
