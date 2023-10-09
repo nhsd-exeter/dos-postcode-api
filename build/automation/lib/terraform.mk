b5309bc2538ec32de8a8076c11fb480281788ecc
Daniel Osae <daniel.osae@nhs.net>
2023-10-09 13:35:24 +0100

    SFD-XXXX Import lambda functions

--- build/automation/lib/terraform.mk
+++ build/automation/lib/terraform.mk
@@ -159,7 +159,9 @@ terraform-import-stack:
 		make _terraform-reinitialise DIR="$(TERRAFORM_DIR)" STACK="$(STACK)"
 	fi
 
-	make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_cloudwatch_log_group.postcode_extract_log_group /aws/lambda/uec-sf-pc-pd-postcode-extract"
+	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_cloudwatch_log_group.postcode_extract_log_group /aws/lambda/uec-sf-pc-pd-postcode-extract"
+	make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_lambda_permission.allow_cloudwatch_to_call_extract_postcode uec-sf-pc-pd-postcode-extract/AllowExecutionFromCloudWatch"
+
 
 # 	make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_lambda_function.postcode_extract_lambda uec-sf-pc-pd-postcode-extract"
 # 	make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_lambda_function.postcode_insert_lambda uec-sf-pc-pd-postcode-insert"
