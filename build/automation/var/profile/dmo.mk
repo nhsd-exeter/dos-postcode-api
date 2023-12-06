db2338a482ecb4d3b4e46def6d6bfc59e8ac7a68
kobl1nhs <142006711+kobl1nhs@users.noreply.github.com>
2023-12-06 16:22:09 +0000

    SFD-5774 Update demo and live environment ETL replica SG and DB (#214)

--- build/automation/var/profile/dmo.mk
+++ build/automation/var/profile/dmo.mk
@@ -52,12 +52,12 @@ TF_VAR_core_dos_python_libs := core-dos-python-libs
 # TF_VAR_postcode_etl_source_db := pathwaysdos_ut
 
 # TF_VAR_sf_read_replica_db  := uec-core-dos-live-db-replica-sf.dos-db-sync-rds
-TF_VAR_sf_read_replica_db_sg := uec-core-dos-live-db-12-replica-sf-sg
+TF_VAR_sf_read_replica_db  := uec-core-dos-live-sf-ro.dos-datastore-live
+TF_VAR_sf_read_replica_db_sg := uec-core-dos-live-datastore-sf-sg
 TF_VAR_dos_read_replica_secret_name := service-finder/deployment
 TF_VAR_dos_read_replica_key := DB_SF_READONLY_PASSWORD
 TF_VAR_postcode_etl_db_user := dos_sf_readonly
 TF_VAR_postcode_etl_source_db := pathwaysdos
-TF_VAR_sf_read_replica_db  := uec-core-dos-live-db-replica-sf.dos-db-sync-rds
 
 
 CERTIFICATE_DOMAIN := certificate
